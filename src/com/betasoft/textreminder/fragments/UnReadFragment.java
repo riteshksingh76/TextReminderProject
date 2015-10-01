d356094116a4dba5599c2f735ae4e7411202ffd4
riteshksingh76 <riteshsingh110992@gmail.com>
2015-10-01 18:07:01 +0530

    first commit

---
+++ src/com/betasoft/textreminder/fragments/UnReadFragment.java
@@ -0,0 +1,28 @@
+package com.betasoft.textreminder.fragments;
+
+import com.betasoft.textreminder.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UnReadFragment  extends Fragment{
+
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		 View rootView = inflater.inflate(R.layout.unread_fragment, container, false);
+		
+		
+		
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
+		
+		
+	}
+	
+}
